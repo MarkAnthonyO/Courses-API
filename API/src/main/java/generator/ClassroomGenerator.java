8219ad259c6520b8bd162d8c420dbd9b627e283b
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-10 20:47:11 -0600

    Corrected incorrect variable names in the Configuration class and gave functionality to the generate method of the ClassroomGenerator class

--- API/src/main/java/generator/ClassroomGenerator.java
+++ API/src/main/java/generator/ClassroomGenerator.java
@@ -1,9 +1,36 @@
 package generator;
 
 import components.Classroom;
+import settings.Settings;
+
+import java.io.*;
 
 public class ClassroomGenerator {
     public static void generate(Classroom classroom) {
+        File classroomFile = new File(Settings.getClassroomPath() + "/" + classroom.getName());
 
+        if (classroomFile.exists()) {
+            System.out.println("The classroom already exists");
+            return;
+        }
+
+        try {
+            FileWriter writer = new FileWriter(classroomFile);
+
+            writer.write("[Classroom]" + "\n");
+            writer.write("Name:" + classroom.getName()+ "\n");
+            writer.write("Capacity:" + classroom.getCapacity() + "\n");
+
+            writer.flush();
+            writer.close();
+
+            if (classroomFile.setReadOnly())
+                System.out.println("Classroom created and insured successfully!");
+            else
+                System.out.println("Classroom created successfully!");
+
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 }
--- API/src/main/java/settings/Settings.java
+++ API/src/main/java/settings/Settings.java
@@ -4,10 +4,26 @@ import java.io.File;
 
 public class Settings {
 
-    private static final File contactsPath = new File(System.getProperty("user.home") + "/Courses/");
+    private static final File projectPath = new File(System.getProperty("user.home") + "/Courses/");
+    private static final File studentsPath = new File(projectPath.getPath() + "/Students/");
+    private static final File classroomPath = new File(projectPath.getPath() + "/Classroom/");
 
-    public static File getContactsPath() {
-        if(!contactsPath.exists()) contactsPath.mkdirs();
-        return contactsPath;
+    public static File getProjectPath() {
+        if(!projectPath.exists())
+            projectPath.mkdirs();
+
+        return projectPath;
+    }
+
+    public static File getStudentsPath() {
+        if(!studentsPath.exists())
+            studentsPath.mkdirs();
+        return studentsPath;
+    }
+
+    public static File getClassroomPath() {
+        if(!classroomPath.exists())
+            classroomPath.mkdirs();
+        return classroomPath;
     }
 }
