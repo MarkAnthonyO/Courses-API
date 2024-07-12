53c10bc6e990c09c9ead2bfe956549dcc5a0cd51
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-11 22:53:21 -0600

    Added SQLiteConnection class

---
+++ API/src/main/java/connection/SQLiteConnection.java
@@ -0,0 +1,41 @@
+package connection;
+
+import java.io.File;
+import java.sql.*;
+
+public class SQLiteConnection {
+
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void startDatabase() {
+        try {
+            if (!new File("client.db").exists()) {
+                connection = DriverManager.getConnection("jdbc:sqlite:client.db");
+                statement = connection.createStatement();
+
+                statement.executeUpdate("CREATE TABLE User(Id INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(125))");
+            } else {
+                connection = DriverManager.getConnection("jdbc:sqlite:client.db");
+                statement = connection.createStatement();
+            }
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static ResultSet query(String query) {
+        ResultSet res = null;
+
+        try {
+            statement.setQueryTimeout(30);
+            res = statement.executeQuery(query);
+        }
+        catch(SQLException e) {
+            e.printStackTrace(System.err);
+        }
+
+        return res;
+    }
+}
