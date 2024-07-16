713f3aede8cef9e7ed6bbf68aacfa49bf37d1a2e
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-15 18:58:39 -0600

    Updated generate method with capacity

--- API/src/main/java/generator/ClassroomGenerator.java
+++ API/src/main/java/generator/ClassroomGenerator.java
@@ -8,7 +8,7 @@ import java.io.*;
 public class ClassroomGenerator {
     public static void generate(Classroom classroom) {
         SQLiteConnection.openConnection();
-	SQLiteConnection.query("INSERT INTO Classroom(name, capacity) values("+ classroom.getName() +")");
+        SQLiteConnection.query("INSERT INTO Classroom(name, capacity) values('" + classroom.getName() + "'," + classroom.getCapacity() + ")");
         SQLiteConnection.closeConnection();
     }
 }
