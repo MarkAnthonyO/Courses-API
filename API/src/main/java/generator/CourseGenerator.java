179bf40f8801b915beba2fc335582c9a9a679fc4
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-15 19:02:08 -0600

    Added the statement to insert course data

--- API/src/main/java/generator/CourseGenerator.java
+++ API/src/main/java/generator/CourseGenerator.java
@@ -1,9 +1,15 @@
 package generator;
 
 import components.Course;
+import connection.SQLiteConnection;
 
 public class CourseGenerator {
     public static void generate(Course course) {
-
+        SQLiteConnection.openConnection();
+        SQLiteConnection.query("INSERT INTO Course(name, idTeacher, idClassroom) values('" +
+                course.getName() + "', " +
+                course.getTeacher().getId() + ", "+
+                course.getClassroom().getId() + ")");
+        SQLiteConnection.closeConnection();
     }
 }
