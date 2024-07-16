684e577c990d2ea96c459dcbf22311171e80b834
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-16 13:47:15 -0600

    Added statement for teacher generate

--- API/src/main/java/generator/TeacherGenerator.java
+++ API/src/main/java/generator/TeacherGenerator.java
@@ -1,9 +1,17 @@
 package generator;
 
 import components.Teacher;
+import connection.SQLiteConnection;
 
 public class TeacherGenerator {
     public static void generate(Teacher teacher) {
-
+        SQLiteConnection.openConnection();
+        SQLiteConnection.query("INSERT INTO Teacher(name, lastname, age, telephone, address) values('" +
+                teacher.getName() + "', '" +
+                teacher.getLastname() + "',"+
+                teacher.getAge() + ", '" +
+                teacher.getTelephone() + "', '" +
+                teacher.getAddress() + "')");
+        SQLiteConnection.closeConnection();
     }
 }
