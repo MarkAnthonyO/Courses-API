6c3b2f19567138a568b9fe897734fa624e1018e8
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-16 16:59:22 -0600

    Added statement for student generation

--- API/src/main/java/generator/StudentGenerator.java
+++ API/src/main/java/generator/StudentGenerator.java
@@ -1,9 +1,17 @@
 package generator;
 
 import components.Student;
+import connection.SQLiteConnection;
 
 public class StudentGenerator {
     public static void generate(Student student) {
-
+        SQLiteConnection.openConnection();
+        SQLiteConnection.query("INSERT INTO Student(name, lastname, age, telephone, address) values('" +
+                student.getName() + "', '" +
+                student.getLastname() + "',"+
+                student.getAge() + ", '" +
+                student.getTelephone() + "', '" +
+                student.getAddress() + "')");
+        SQLiteConnection.closeConnection();
     }
 }
