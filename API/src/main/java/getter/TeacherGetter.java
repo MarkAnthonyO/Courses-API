81257884a97cd666a8cea0822cf3c79908626835
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-16 18:14:22 -0600

    Created TeacherGetter class

---
+++ API/src/main/java/getter/TeacherGetter.java
@@ -0,0 +1,25 @@
+package getter;
+
+import components.Teacher;
+import connection.SQLiteConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class TeacherGetter {
+    public static Teacher get(int id) {
+        SQLiteConnection.openConnection();
+        ResultSet rs = SQLiteConnection.query("SELECT * FROM Teacher WHERE Id=" + id);
+        Teacher teacher = null;
+
+        try {
+            while (rs.next()) {
+                teacher =  new Teacher(rs.getInt("Id"), rs.getString("name"), rs.getString("lastname"), rs.getInt("age"), rs.getString("telephone"), rs.getString("address"));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return teacher;
+    }
+}
