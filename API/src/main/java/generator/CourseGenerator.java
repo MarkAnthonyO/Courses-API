1b70d23b9f835b20af5be05201934bf7c26c8d70
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:26:51 -0600

    Update CourseGenerator.java

--- API/src/main/java/generator/CourseGenerator.java
+++ API/src/main/java/generator/CourseGenerator.java
@@ -3,9 +3,20 @@ package generator;
 import components.Course;
 import connection.SQLiteConnection;
 
-import java.sql.ResultSet;
-
+/**
+ * <h1>CourseGenerator class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class CourseGenerator {
+    /**
+     * <h1>Generate course</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * CourseGenerator.generate(course);
+     * }</pre>
+     * @param course course to generate
+     */
     public static void generate(Course course) {
         SQLiteConnection.query("INSERT INTO Course(name, idTeacher, idClassroom) values('" +
                 course.getName() + "', " +
