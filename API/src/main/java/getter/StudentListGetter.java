e43fcf77956e642b2c8fb184fe3ac8709d08e1b0
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:25:44 -0600

    Update StudentListGetter.java

--- API/src/main/java/getter/StudentListGetter.java
+++ API/src/main/java/getter/StudentListGetter.java
@@ -7,11 +7,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * <h1>StudentListGetter class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class StudentListGetter {
+
+    /**
+     * <h1>Get students from course</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * ArrayList<Student> students = StudentListGetter.get(1);
+     * }</pre>
+     * @param courseId id course
+     * @return All students from course
+     */
     public static ArrayList<Student> get(int courseId) {
         ArrayList<Student> students = new ArrayList<>();
         ResultSet rs = SQLiteConnection.query("SELECT * FROM StudentCourse WHERE IdCourse = " + courseId);
-
 
         try {
             while (rs.next()) {
