ca21b27af36783c0a3256a36e3488e0e34b68c2f
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:25:42 -0600

    Update CourseGetter.java

--- API/src/main/java/getter/CourseGetter.java
+++ API/src/main/java/getter/CourseGetter.java
@@ -1,16 +1,28 @@
 package getter;
 
-import components.Classroom;
 import components.Course;
-import components.Student;
-import components.Teacher;
 import connection.SQLiteConnection;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * <h1>CourseGetter class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class CourseGetter {
+
+    /**
+     * <h1>Get course</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * Course course = CourseGetter.get(1);
+     * }</pre>
+     * @param id id course
+     * @return course
+     */
     public static Course get(int id) {
         Course course = null;
         ResultSet rs = SQLiteConnection.query("SELECT * FROM Course WHERE Id=" + id);
@@ -26,5 +38,31 @@ public class CourseGetter {
             System.out.println(ex.getMessage());
         }
         return course;
+    }
+
+    /**
+     * <h1>Get courses</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * ArrayList<Course> courses = CourseGetter.getAll();
+     * }</pre>
+     * @return All courses
+     */
+    public static ArrayList<Course> getAll() {
+        ResultSet rs = SQLiteConnection.query("SELECT * FROM Course");
+        ArrayList<Course> courses = new ArrayList<>();
+
+        try {
+            while (rs.next()) {
+                int id = rs.getInt("Id");
+                String name = rs.getString("name");
+                int idTeacher = rs.getInt("IdTeacher");
+                int idClassroom = rs.getInt("IdClassroom");
+                courses.add(new Course(id, name, TeacherGetter.get(idTeacher), StudentListGetter.get(id), ClassroomGetter.get(idClassroom)));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return courses;
     }
 }
