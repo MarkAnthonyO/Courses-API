5b5a87f91a2483c1f5883c92e19aeed402e158e8
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:27:00 -0600

    Update ClassroomGetter.java

--- API/src/main/java/getter/ClassroomGetter.java
+++ API/src/main/java/getter/ClassroomGetter.java
@@ -5,8 +5,24 @@ import connection.SQLiteConnection;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+/**
+ * <h1>ClassroomGetter class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class ClassroomGetter {
+
+    /**
+     * <h1>Get classroom</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * Classroom classroom = ClassroomGetter.get(1);
+     * }</pre>
+     * @param id id student
+     * @return classroom
+     */
     public static Classroom get(int id) {
         ResultSet rs = SQLiteConnection.query("SELECT * FROM Classroom WHERE Id=" + id);
         Classroom classroom = null;
@@ -20,5 +36,28 @@ public class ClassroomGetter {
         }
 
         return classroom;
+    }
+
+    /**
+     * <h1>Get all classroom</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * ArrayList<Classroom> classrooms = ClassroomGetter.getAll();
+     * }</pre>
+     * @return All classrooms
+     */
+    public static ArrayList<Classroom> getAll() {
+        ResultSet rs = SQLiteConnection.query("SELECT * FROM Classroom");
+        ArrayList<Classroom> students = new ArrayList<>();
+
+        try {
+            while (rs.next()) {
+                students.add(new Classroom(rs.getInt("Id"), rs.getString("name"), rs.getInt("capacity")));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        return students;
     }
 }
