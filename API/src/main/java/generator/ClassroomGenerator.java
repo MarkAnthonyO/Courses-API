1ff5b4902dae075f38e0438b0ebd5963e4dd82b8
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:26:49 -0600

    Update ClassroomGenerator.java

--- API/src/main/java/generator/ClassroomGenerator.java
+++ API/src/main/java/generator/ClassroomGenerator.java
@@ -3,10 +3,21 @@ package generator;
 import components.Classroom;
 import connection.SQLiteConnection;
 
-import java.io.*;
-import java.sql.ResultSet;
-
+/**
+ * <h1>ClassroomGenerator class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class ClassroomGenerator {
+
+    /**
+     * <h1>Generate classroom</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * ClassroomGenerator.generate(classroom);
+     * }</pre>
+     * @param classroom classroom to generate
+     */
     public static void generate(Classroom classroom) {
         SQLiteConnection.query("INSERT INTO Classroom(name, capacity) values('" + classroom.getName() + "'," + classroom.getCapacity() + ")");
     }
