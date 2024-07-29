2801d20d063ccf9903852ac075daec0d531161d9
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:26:53 -0600

    Update StudentGenerator.java

--- API/src/main/java/generator/StudentGenerator.java
+++ API/src/main/java/generator/StudentGenerator.java
@@ -3,9 +3,21 @@ package generator;
 import components.Student;
 import connection.SQLiteConnection;
 
-import java.sql.ResultSet;
-
+/**
+ * <h1>StudentGenerator class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class StudentGenerator {
+
+    /**
+     * <h1>Generate Student</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * StudentGenerator.generate(student);
+     * }</pre>
+     * @param student student to generate
+     */
     public static void generate(Student student) {
         SQLiteConnection.query("INSERT INTO Student(name, lastname, age, telephone, address) values('" +
                 student.getName() + "', '" +
