a1347d602263112b52c4506eebe0b01668182bd3
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-07-29 17:25:39 -0600

    Update StudentGetter.java

--- API/src/main/java/getter/StudentGetter.java
+++ API/src/main/java/getter/StudentGetter.java
@@ -1,13 +1,28 @@
 package getter;
 
-import components.Classroom;
 import components.Student;
 import connection.SQLiteConnection;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+/**
+ * <h1>CourseGetter class</h1>
+ * @author MarkAnthonyO
+ * @version v1.0.0
+ */
 public class StudentGetter {
+
+    /**
+     * <h1>Get student</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * Student student = StudentGetter.get(1);
+     * }</pre>
+     * @param id id student
+     * @return student
+     */
     public static Student get(int id) {
         ResultSet rs = SQLiteConnection.query("SELECT * FROM Student WHERE Id=" + id);
         Student student = null;
@@ -20,5 +35,27 @@ public class StudentGetter {
             System.out.println(ex.getMessage());
         }
         return student;
+    }
+
+    /**
+     * <h1>Get students</h1>
+     * <h3>Example of use</h3>
+     * <pre>{@code
+     * ArrayList<Student> students = StudentGetter.getAll();
+     * }</pre>
+     * @return All students
+     */
+    public static ArrayList<Student> getAll() {
+        ResultSet rs = SQLiteConnection.query("SELECT * FROM Student");
+        ArrayList<Student> students = new ArrayList<>();
+
+        try {
+            while (rs.next()) {
+                students.add(new Student(rs.getInt("Id"), rs.getString("name"), rs.getString("lastname"), rs.getInt("age"), rs.getString("telephone"), rs.getString("address")));
+            }
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return students;
     }
 }
