3a3e2d90086e5557338ba8a839bc310615d08888
MarkAnthonyO <87096269+MarkAnthonyO@users.noreply.github.com>
2024-08-06 10:30:11 -0600

    Update StudentDeleter.java

--- API/src/main/java/deleter/StudentDeleter.java
+++ API/src/main/java/deleter/StudentDeleter.java
@@ -6,6 +6,7 @@ public class StudentDeleter {
     public static void delete(int id) {
         SQLiteConnection.openConnection();
         SQLiteConnection.query("DELETE FROM Student WHERE Id=" + id);
+        SQLiteConnection.query("DELETE FROM StudentCourse WHERE IdStudent=" + id);
         SQLiteConnection.closeConnection();
     }
 }
